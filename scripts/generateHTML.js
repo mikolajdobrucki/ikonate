924cdd8cf4709c68990c29323ae6a9dc89bd62ef
mdusza <dmarcin1993@tlen.pl>
2018-05-16 09:14:00 +0200

    dynamic demo HTML generation

---
+++ scripts/generateHTML.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const jsdom = require('jsdom');
+const JSDOM = jsdom.JSDOM;
+
+
+fs.readdir('./icons', (err, files) => {
+  let result = files.map((item) => {
+    return `
+      <div class="container__grid-item">const
+        <svg class="customicons">
+          <use xlink:href="../sprite/${item}#anchor"/>
+        </svg>
+      </div>`;
+  });
+  fs.readFile('./demo/index.html', 'utf8', (err, data) => {
+    const dom = new JSDOM(data);
+    const element = dom.window.document.getElementsByClassName('container')[0];
+    element.innerHTML = result.join('');
+    fs.writeFile('./demo/index.html', `<!DOCTYPE html>` + dom.window.document.documentElement.outerHTML)
+  });
+});
