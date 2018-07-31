9618b3e37ad61aba9018627e1a823212e38cb256
Marcin Zaremski <marcinzara@gmail.com>
2018-07-31 13:29:32 +0200

    Added callback to fs function

--- scripts/spritesDir.js
+++ scripts/spritesDir.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 fs.access('sprite', fs.constants.F_OK, (err) => {
   if (err) {
-    fs.mkdir('sprite');
+    fs.mkdir('sprite', (err)=>{if (err){console.error(err)}});
   }
 });
