6ffeb6e6cb1b5012426e9562874dd84b2a4fdc66
Marcin Zaremski <marcinzara@gmail.com>
2018-08-01 16:11:32 +0200

    Change method of generate demo html

--- scripts/generateHTML.js
+++ scripts/generateHTML.js
@@ -1,69 +1,26 @@
 const fs = require('fs');
+const pretty = require('pretty')
 const jsdom = require('jsdom');
 const JSDOM = jsdom.JSDOM;
 
-const baseHTML = `
-<html lang="en" dir="ltr"><head>
-    <meta charset="utf-8">
-    <title>Custom Icons</title>
-  <style>
-    :root {
-      /* Colour Palette */
-      --grey--medium:   #9ea7c7;
-      --grey--light:    #fafafd;
-      --white:          #ffffff;
-      --brand:          #0E3FF2;
 
-      /* Dimensions */
-      --border-radius:  4px;
-    }
-    body {
-      background: var(--grey--light);
-    }
-    .container {
-      display: grid;
-      grid-template-columns: repeat(12,1fr);
-      width: 1152px;
-      grid-gap: 32px;
-      margin: 0 auto;
-      color: var(--brand);
-    }
-    .container__grid-item {
-      background: var(--white);
-      border-radius: var(--border-radius);
-      padding: 20px;
-    }
-    .custom-icons {
-      width: 24px;
-      height: 24px;
-      stroke: currentColor;
-      stroke-width: 2;
-      stroke-linecap: round;
-      stroke-linejoin: round;
-      fill: none;
-    }
-  </style></head>
+const DOM = new JSDOM(fs.readFileSync("IndexTemplate.html").toString("utf-8"))
+const Document = DOM.window.document
 
-  <body>
-    <div class="container"></div>
-  </body>
-</html>
-`;
 
 fs.readdir('./icons', (err, files) => {
   let result = files.map((item) => {
-    return `
-      <div class="container__grid-item">
-        <svg class="custom-icons">
-          <use xlink:href="../sprite/custom-icons.svg#${item.replace('.svg','')}"/>
-        </svg>
-      </div>`;
+      const svg = new JSDOM(fs.readFileSync("./icons/" + item).toString("utf-8")).window.document.getElementsByTagName("svg")["0"]
+
+      const div = Document.createElement("div")
+      div.classList.add("container__grid-item")
+
+      div.appendChild(svg)
+      Document.getElementsByClassName("container")["0"].appendChild(div)
   });
-  const dom = new JSDOM(baseHTML);
-  const element = dom.window.document.getElementsByClassName('container')[0];
-  element.innerHTML = result.join('');
-  fs.mkdir('demo', () => {
-    fs.writeFile('demo/index.html', `<!DOCTYPE html>` + dom.window.document.documentElement.outerHTML, (err) => {
+
+   fs.mkdir('demo', () => {
+    fs.writeFile('demo/index.html', pretty(DOM.serialize()) , (err) => {
       if (err) {
         console.log(err);
       }
