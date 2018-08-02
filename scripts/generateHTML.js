0ead417bcf65244727fab51cb6e292ecd07b67ff
Marcin Zaremski <marcinzara@gmail.com>
2018-08-02 16:16:43 +0200

    generateHTML prettied

--- scripts/generateHTML.js
+++ scripts/generateHTML.js
@@ -1,29 +1,29 @@
-const fs = require('fs');
-const pretty = require('pretty')
-const jsdom = require('jsdom');
+const fs = require("fs");
+const pretty = require("pretty");
+const jsdom = require("jsdom");
 const JSDOM = jsdom.JSDOM;
 
+const DOM = new JSDOM(fs.readFileSync("IndexTemplate.html").toString("utf-8"));
+const Document = DOM.window.document;
 
-const DOM = new JSDOM(fs.readFileSync("IndexTemplate.html").toString("utf-8"))
-const Document = DOM.window.document
+fs.readdir("./icons", (err, files) => {
+    let result = files.map(item => {
+        const svg = new JSDOM(
+            fs.readFileSync("./icons/" + item).toString("utf-8")
+        ).window.document.getElementsByTagName("svg")["0"];
 
+        const div = Document.createElement("div");
+        div.classList.add("container__grid-item");
 
-fs.readdir('./icons', (err, files) => {
-  let result = files.map((item) => {
-      const svg = new JSDOM(fs.readFileSync("./icons/" + item).toString("utf-8")).window.document.getElementsByTagName("svg")["0"]
+        div.appendChild(svg);
+        Document.getElementsByClassName("container")["0"].appendChild(div);
+    });
 
-      const div = Document.createElement("div")
-      div.classList.add("container__grid-item")
-
-      div.appendChild(svg)
-      Document.getElementsByClassName("container")["0"].appendChild(div)
-  });
-
-   fs.mkdir('demo', () => {
-    fs.writeFile('demo/index.html', pretty(DOM.serialize()) , (err) => {
-      if (err) {
-        console.log(err);
-      }
-    })
-  });
+    fs.mkdir("demo", () => {
+        fs.writeFile("demo/index.html", pretty(DOM.serialize()), err => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
 });
