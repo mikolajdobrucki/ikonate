6e07237a8971fa8e40ad1998a4b660027e1524e1
Marcin Zaremski <marcinzara@gmail.com>
2018-08-03 16:40:10 +0200

    small changes

--- scripts/generateHTML.js
+++ scripts/generateHTML.js
@@ -33,54 +33,53 @@ module.exports = (params) => {
 }
 
 
-function generateInline(files, Document){
-        console.log("Generating INLINE demo")
-        // <div class="container__grid-item">
-        //     <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="alignCenterIconTitle alignCenterIconDesc">
-        //         <title id="alignCenterIconTitle">Align Center</title>
-        //         <desc id="alignCenterIconDesc">Icon of four horizontal lines representing central alignment of text</desc>
-        //         <path d="M8 10L16 10M6 6L18 6M6 14L18 14M8 18L16 18" />
-        //     </svg>
-        // </div>;
+function generateInline(icons, Document){
+    console.log("Generating INLINE demo")
+    // <div class="container__grid-item">
+    //     <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="alignCenterIconTitle alignCenterIconDesc">
+    //         <title id="alignCenterIconTitle">Align Center</title>
+    //         <desc id="alignCenterIconDesc">Icon of four horizontal lines representing central alignment of text</desc>
+    //         <path d="M8 10L16 10M6 6L18 6M6 14L18 14M8 18L16 18" />
+    //     </svg>
+    // </div>;
 
-        files.map(item => {
-            const svgFile = new JSDOM(
-                fs.readFileSync("./icons/" + item).toString("utf-8")
-            ).window.document.getElementsByTagName("svg")["0"];
+    icons.map(item => {
+        const svgFile = new JSDOM(
+            fs.readFileSync("./icons/" + item).toString("utf-8")
+        ).window.document.getElementsByTagName("svg")["0"];
 
-            const div = Document.createElement("div");
-            div.classList.add("container__grid-item");
+        const div = Document.createElement("div");
+        div.classList.add("container__grid-item");
 
-            div.appendChild(svgFile);
-            Document.getElementsByClassName("container")["0"].appendChild(div);
-        });
+        div.appendChild(svgFile);
+        Document.getElementsByClassName("container")["0"].appendChild(div);
+    });
 }
 
 
-function generateSprite(files, Document){
-        console.log("Generating SPRITESHEETS demo")
+function generateSprite(icons, Document){
+    console.log("Generating SPRITESHEETS demo")
 
-        // <div class="container__grid-item">
-        //     <svg class="custom-icons">
-        //       <use xlink:href="../sprite/custom-icons.svg#activity"></use>
-        //     </svg>
-        // </div>
+    // <div class="container__grid-item">
+    //     <svg class="custom-icons">
+    //       <use xlink:href="../sprite/custom-icons.svg#activity"></use>
+    //     </svg>
+    // </div>
 
-        files.map(item => {
-            const div = Document.createElement("div");
-            div.
-            classList.add("container__grid-item");
+    icons.map(item => {
+        const div = Document.createElement("div");
+        div.classList.add("container__grid-item");
 
-            const svg = Document.createElement("svg");
-            svg.classList.add("custom-icons");
+        const svg = Document.createElement("svg");
+        svg.classList.add("custom-icons");
 
-            const use = Document.createElement("use");
-            use.setAttribute("xlink:href", "../sprite/custom-icons.svg#"+item.replace(".svg", ""))
+        const use = Document.createElement("use");
+        use.setAttribute("xlink:href", "../sprite/custom-icons.svg#"+item.replace(".svg", ""))
 
-            svg.appendChild(use);
-            div.appendChild(svg);
-            Document.getElementsByClassName("container")["0"].appendChild(div);
-        });
+        svg.appendChild(use);
+        div.appendChild(svg);
+        Document.getElementsByClassName("container")["0"].appendChild(div);
+    });
 }
 
 
